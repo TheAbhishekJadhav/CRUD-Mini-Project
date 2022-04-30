45b8e7f825c6ad39eb4e9558405e9f5c4e055ed1
Abhishek Jadhav <89737249+TheAbhishekJadhav@users.noreply.github.com>
2022-04-30 03:47:36 +0200

    removed secret

--- djminip/mydjproj/mydjproj/settings.py
+++ djminip/mydjproj/mydjproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aj(zljd)%0uow@h!tez5rj+5*o0ypdt+7%(k&ci)8hoh7u_wrb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
